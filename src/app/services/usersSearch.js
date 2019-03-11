3a9c6b37b9eda32aebdb6a31752b92840ca3fa19
Rhys Bridges <bridgesr@uni.coventry.ac.uk>
2019-03-11 13:51:06 +0000

    NSA-1922- manage users

---
+++ src/app/services/usersSearch.js
@@ -0,0 +1,91 @@
+const { searchForUsersForService } = require('./../../infrastructure/search');
+
+const search = async (req) => {
+  const serviceId = req.params.sid;
+  const paramsSource = req.method === 'POST' ? req.body : req.query;
+
+  let criteria = paramsSource.criteria;
+  if (!criteria) {
+    criteria = '';
+  }
+  let safeCriteria = criteria;
+  if (criteria.indexOf('-') !== -1) {
+    criteria = "\"" + criteria + "\"";
+  }
+
+  let page = paramsSource.page ? parseInt(paramsSource.page) : 1;
+  if (isNaN(page)) {
+    page = 1;
+  }
+
+  let sortBy = paramsSource.sort ? paramsSource.sort.toLowerCase() : 'name';
+  let sortAsc = (paramsSource.sortdir ? paramsSource.sortdir : 'asc').toLowerCase() === 'asc';
+
+  const results = await searchForUsersForService(serviceId, criteria + '*', page, sortBy, sortAsc ? 'asc' : 'desc');
+  //TODO : audit search
+
+  return {
+    criteria: safeCriteria,
+    page,
+    sortBy,
+    sortOrder: sortAsc ? 'asc' : 'desc',
+    numberOfPages: results.numberOfPages,
+    totalNumberOfResults: results.totalNumberOfResults,
+    users: results.users,
+    sort: {
+      name: {
+        nextDirection: sortBy === 'name' ? (sortAsc ? 'desc' : 'asc') : 'asc',
+        applied: sortBy === 'name',
+      },
+      email: {
+        nextDirection: sortBy === 'email' ? (sortAsc ? 'desc' : 'asc') : 'asc',
+        applied: sortBy === 'email',
+      },
+      organisation: {
+        nextDirection: sortBy === 'organisation' ? (sortAsc ? 'desc' : 'asc') : 'asc',
+        applied: sortBy === 'organisation',
+      },
+      lastLogin: {
+        nextDirection: sortBy === 'lastlogin' ? (sortAsc ? 'desc' : 'asc') : 'asc',
+        applied: sortBy === 'lastlogin',
+      },
+      status: {
+        nextDirection: sortBy === 'status' ? (sortAsc ? 'desc' : 'asc') : 'asc',
+        applied: sortBy === 'status',
+      },
+    }
+  }
+};
+
+const viewModel = async (req) => {
+  const result = await search(req);
+
+  return {
+    csrfToken: req.csrfToken(),
+    serviceId: req.params.sid,
+    backLink: true,
+    criteria: result.criteria,
+    page: result.page,
+    numberOfPages: result.numberOfPages,
+    totalNumberOfResults: result.totalNumberOfResults,
+    users: result.users,
+    sort: result.sort,
+    sortBy: result.sortBy,
+    sortOrder: result.sortOrder,
+  };
+};
+
+const get = async (req, res) => {
+  const model = await viewModel(req);
+  return res.render('services/views/usersSearch', model);
+};
+
+const post = async (req, res) => {
+  const model = await viewModel(req);
+  return res.render('services/views/usersSearch', model);
+};
+
+module.exports = {
+  get,
+  post,
+};
---
+++ src/infrastructure/search/api.js
@@ -0,0 +1,95 @@
+const jwtStrategy = require('login.dfe.jwt-strategies');
+const config = require('./../config');
+const KeepAliveAgent = require('agentkeepalive').HttpsAgent;
+const rp = require('login.dfe.request-promise-retry').defaults({
+  agent: new KeepAliveAgent({
+    maxSockets: config.hostingEnvironment.agentKeepAlive.maxSockets,
+    maxFreeSockets: config.hostingEnvironment.agentKeepAlive.maxFreeSockets,
+    timeout: config.hostingEnvironment.agentKeepAlive.timeout,
+    keepAliveTimeout: config.hostingEnvironment.agentKeepAlive.keepAliveTimeout,
+  }),
+});
+const { mapUserStatus } = require('./../../infrastructure/utils');
+
+const callApi = async (endpoint, method, body, correlationId) => {
+  const token = await jwtStrategy(config.search.service).getBearerToken();
+
+  try {
+    return await rp({
+      method: method,
+      uri: `${config.search.service.url}${endpoint}`,
+      headers: {
+        authorization: `bearer ${token}`,
+        'x-correlation-id': correlationId,
+      },
+      body: body,
+      json: true,
+      strictSSL: config.hostingEnvironment.env.toLowerCase() !== 'dev',
+    });
+  } catch (e) {
+    const status = e.statusCode ? e.statusCode : 500;
+    if (status === 404) {
+      return undefined;
+    }
+    throw e;
+  }
+};
+
+const mapSearchUserToSupportModel = (user) => {
+  return {
+    id: user.id,
+    name: `${user.firstName} ${user.lastName}`,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    email: user.email,
+    organisation: user.primaryOrganisation ? {
+      name: user.primaryOrganisation
+    } : null,
+    organisations: user.organisations,
+    lastLogin: user.lastLogin ? new Date(user.lastLogin) : null,
+    successfulLoginsInPast12Months: user.numberOfSuccessfulLoginsInPast12Months,
+    status: mapUserStatus(user.statusId, user.statusLastChangedOn),
+    pendingEmail: user.pendingEmail,
+  };
+};
+
+const mapSupportUserSortByToSearchApi = (supportSortBy) => {
+  switch (supportSortBy.toLowerCase()) {
+    case 'name':
+      return 'searchableName';
+    case 'email':
+      return 'searchableEmail';
+    case 'organisation':
+      return 'primaryOrganisation';
+    case 'lastlogin':
+      return 'lastLogin';
+    case 'status':
+      return 'statusId';
+    default:
+      throw new Error(`Unexpected user sort field ${supportSortBy}`);
+  }
+};
+
+const searchForUsersForService = async (serviceId, criteria, pageNumber, sortBy, sortDirection) => {
+  try {
+    let endpoint = `/users?filter_services=${serviceId}&criteria=${criteria}&page=${pageNumber}`;
+    if (sortBy) {
+      endpoint += `&sortBy=${mapSupportUserSortByToSearchApi(sortBy)}`;
+    }
+    if (sortDirection) {
+      endpoint += `&sortDirection=${sortDirection}`;
+    }
+    const results = await callApi(endpoint, 'GET');
+    return {
+      numberOfPages: results.numberOfPages,
+      totalNumberOfResults: results.totalNumberOfResults,
+      users: results.users.map(mapSearchUserToSupportModel)
+    }
+  } catch (e) {
+    throw new Error(`Error searching for users with criteria ${criteria} (page: ${pageNumber}) - ${e.message}`);
+  }
+};
+
+module.exports = {
+  searchForUsersForService,
+};
