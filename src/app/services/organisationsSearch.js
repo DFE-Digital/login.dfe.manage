8f85a1907ec5bd0e5f85691360462c1a2aa337b0
Rhys Bridges <bridgesr@uni.coventry.ac.uk>
2019-03-13 16:06:09 +0000

    NSA-2020- view organisations

---
+++ src/app/services/organisationsSearch.js
@@ -0,0 +1,35 @@
+const { searchOrganisations } = require('./../../infrastructure/organisations');
+
+const buildModel = async (req) => {
+  const inputSource = req.method.toUpperCase() === 'POST' ? req.body : req.query;
+  const criteria = inputSource.criteria || '';
+  let pageNumber = parseInt(inputSource.page) || 1;
+  if (isNaN(pageNumber)) {
+    pageNumber = 1;
+  }
+
+  const pageOfOrganisations = await searchOrganisations(criteria, undefined, pageNumber, req.id);
+  return {
+    csrfToken: req.csrfToken(),
+    criteria: criteria,
+    page: pageNumber,
+    numberOfPages: pageOfOrganisations.totalNumberOfPages,
+    totalNumberOfResults: pageOfOrganisations.totalNumberOfRecords,
+    organisations: pageOfOrganisations.organisations,
+  }
+};
+
+const get = async (req, res) => {
+  const model = await buildModel(req);
+  return res.render('services/views/organisationsSearch', model);
+};
+
+const post = async (req, res) => {
+  const model = await buildModel(req);
+  return res.render('services/views/organisationsSearch', model);
+};
+
+module.exports = {
+  get,
+  post,
+};
