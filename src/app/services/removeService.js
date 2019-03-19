bd397ecca527b225a05d25be81ce18628a72ae04
Rhys Bridges <bridgesr@uni.coventry.ac.uk>
2019-03-19 15:48:03 +0000

    audit remove service

--- src/app/services/removeService.js
+++ src/app/services/removeService.js
@@ -2,6 +2,7 @@ const { removeServiceFromUser, removeServiceFromInvitation } = require('./../../
 const { getServiceById } = require('./../../infrastructure/applications');
 const { getUserDetails } = require('./utils');
 const { getOrganisationByIdV2 } = require('./../../infrastructure/organisations');
+const logger = require('./../../infrastructure/logger');
 
 const getModel = async (req) => {
   const service = await getServiceById(req.params.sid, req.id);
@@ -28,7 +29,18 @@ const post = async (req, res) => {
   req.params.uid.startsWith('inv-') ? await removeServiceFromInvitation(req.params.uid.substr(4), req.params.sid, req.params.oid, req.id) :
     await removeServiceFromUser(req.params.uid, req.params.sid, req.params.oid, req.id);
 
-  //TODO: audit remove service
+  logger.audit(`${req.user.email} (id: ${req.user.sub}) removed service ${model.service.name} for organisation ${model.organisation.name} (id: ${model.organisation.id}) for user ${model.user.email} (id: ${model.user.id})`, {
+    type: 'manage',
+    subType: 'user-service-deleted',
+    userId: req.user.sub,
+    userEmail: req.user.email,
+    editedUser: model.user.id,
+    editedFields: [{
+      name: 'remove_service',
+      oldValue: model.service.id,
+      newValue: undefined,
+    }],
+  });
 
   res.flash('info', `${model.user.firstName} ${model.user.lastName} removed from ${model.service.name}`);
   return res.redirect(`/services/${req.params.sid}/users/${req.params.uid}/organisations`);
