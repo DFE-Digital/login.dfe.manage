f893e928d9462eeda282c239d40f78e8c7213f2f
Sumanta Sarkar <sumanta.sr@gmail.com>
2021-04-06 14:30:42 +0100

    Release/1.0.1 (#91)
    * NSA-3272
    
    * node 12 upgrade
    
    * Added cookie banner and version for static assets
    
    * passing version to error handling component
    
    * Removed cookie banner
    
    * Removed GA
    
    * Original cookie banner back where it was
    
    * GA is back
    
    * Added vendors and GA tracking id
    
    * added link to privacy notice
    
    * Trim search criteria and remove html encode for it
    
    * Showing error message when user already mapped to that organisation
    
    * Updates ejs, runs audit fixes
    
    Co-authored-by: jovin-joy <jovin.joy@education.gov.uk>
    Co-authored-by: jovin-joy <57987577+jovin-joy@users.noreply.github.com>
    Co-authored-by: Jose Luis Garcia Ramos <joseluisgraa@gmail.com>
    Co-authored-by: Matt Cavanagh <matt.c@darkynt.co.uk>

--- src/index.js
+++ src/index.js
@@ -16,25 +16,13 @@ const oidc = require('./infrastructure/oidc');
 const session = require('cookie-session');
 const { setUserContext, isManageUser } = require('./infrastructure/utils');
 const { getErrorHandler, ejsErrorPages } = require('login.dfe.express-error-handling');
-const KeepAliveAgent = require('agentkeepalive');
 const moment = require('moment');
 
 const registerRoutes = require('./routes');
 
 configSchema.validate();
 
-http.GlobalAgent = new KeepAliveAgent({
-  maxSockets: config.hostingEnvironment.agentKeepAlive.maxSockets,
-  maxFreeSockets: config.hostingEnvironment.agentKeepAlive.maxFreeSockets,
-  timeout: config.hostingEnvironment.agentKeepAlive.timeout,
-  keepAliveTimeout: config.hostingEnvironment.agentKeepAlive.keepAliveTimeout,
-});
-https.GlobalAgent = new KeepAliveAgent({
-  maxSockets: config.hostingEnvironment.agentKeepAlive.maxSockets,
-  maxFreeSockets: config.hostingEnvironment.agentKeepAlive.maxFreeSockets,
-  timeout: config.hostingEnvironment.agentKeepAlive.timeout,
-  keepAliveTimeout: config.hostingEnvironment.agentKeepAlive.keepAliveTimeout,
-});
+https.globalAgent.maxSockets = http.globalAgent.maxSockets = config.hostingEnvironment.agentKeepAlive.maxSockets || 50;
 
 
 const init = async () => {
@@ -52,7 +40,7 @@ const init = async () => {
     },
   }));
 
-  let assetsUrl = config.hostingEnvironment.assetsUrl || 'https://rawgit.com/DFE-Digital/dfe.ui.toolkit/master/dist/';
+  let assetsUrl = config.assets.url;
   assetsUrl = assetsUrl.endsWith('/') ? assetsUrl.substr(0, assetsUrl.length - 1) : assetsUrl;
   Object.assign(app.locals, {
     moment,
@@ -67,6 +55,10 @@ const init = async () => {
     app: {
       title: 'DfE Sign-in Manage',
       environmentBannerMessage: config.hostingEnvironment.environmentBannerMessage,
+    },
+    gaTrackingId: config.hostingEnvironment.gaTrackingId,
+    assets: {
+      version: config.assets.version,
     },
   });
 
@@ -92,9 +84,18 @@ const init = async () => {
 
   app.use(flash());
 
-  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(bodyParser.urlencoded({ extended: true }));
   app.use(cookieParser());
-  app.use(sanitization());
+  app.use(sanitization({
+    sanitizer: (key, value) => {
+      //add exception for fields that we don't want to encode
+      const fieldToNotSanitize = ['criteria'];
+      if (fieldToNotSanitize.find(x => x.toLowerCase() === key.toLowerCase())) {
+        return value;
+      }
+      return sanitization.defaultSanitizer(key, value);
+    },
+  }));
   app.set('view engine', 'ejs');
   app.set('views', path.resolve(__dirname, 'app'));
   app.use(expressLayouts);
@@ -110,6 +111,7 @@ const init = async () => {
   const errorPageRenderer = ejsErrorPages.getErrorPageRenderer({
     help: config.hostingEnvironment.helpUrl,
     assets: assetsUrl,
+    assetsVersion: config.assets.version,
   }, config.hostingEnvironment.env === 'dev');
   app.use(getErrorHandler({
     logger,
