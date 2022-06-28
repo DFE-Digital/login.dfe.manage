792d8c871d7aa8fbb978bd435a3a39ede33dbd38
Ronald Repka <ronald.repka@gmail.com>
2022-06-28 15:45:19 +0100

    NSA-6143 Update Manage policies page to latest GDS

--- src/app/services/getPolicyRoles.js
+++ src/app/services/getPolicyRoles.js
@@ -4,11 +4,21 @@ const { getServiceById } = require('./../../infrastructure/applications');
 const getPolicyRoles= async (req, res) => {
   const service = await  getServiceById(req.params.sid, req.id);
   const policy = await getPolicyById(req.params.sid, req.params.pid, req.id);
+  const allUserRoles = req.userServices.roles.map((role) => ({
+    serviceId: role.code.substr(0, role.code.indexOf('_')),
+    role: role.code.substr(role.code.lastIndexOf('_') + 1),
+  }));
+  const userRolesForService = allUserRoles.filter(x => x.serviceId === req.params.sid);
+  const manageRolesForService = userRolesForService.map(x => x.role);
+
   return res.render('services/views/policyRoles', {
     csrfToken: req.csrfToken(),
     policy,
     service,
     backLink: `/services/${req.params.sid}/policies`,
+    serviceId: req.params.sid,
+    userRoles: manageRolesForService,
+    currentPage: 'policies',
   })
 };
 
