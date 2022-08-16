13fcd960b0ec1679a4c7fc1c11b4a980e7df5648
Ionut Predoi <ipredoi27@gmail.com>
2022-08-16 11:17:37 +0100

    NSA-5783 - unit tests updated

--- src/infrastructure/access/static.js
+++ src/infrastructure/access/static.js
@@ -9,73 +9,51 @@ const services = [
         code: 'test',
         status: {
           id: 1,
-        }
-      }
-    ]
-  }
-];
-const getServicesForUser  = (id, correlationId) => {
-  const services = services.find(x => x.userId.toLowerCase() === id.toLowerCase());
-  return Promise.resolve(services);
-};
-
-const getSingleUserService = (id, sid, oid, correlationId) => {
-  return Promise.resolve([]);
-};
-
-const getSingleInvitationService = (iid, sid, oid, correlationId) => {
-  return Promise.resolve([]);
-};
-
-const listRolesOfService = async (sid, correlationId) => {
-  return Promise.resolve([]);
-};
-
-const updateUserService = async (uid, sid, oid, role, correlationId) => {
-  return Promise.resolve(null);
-};
-
-const updateInvitationService = async (iid, sid, oid, role, correlationId) => {
-  return Promise.resolve(null);
-};
-
-const removeServiceFromUser = async(uid, sid, oid, correlationId) => {
-  return Promise.resolve();
-};
-
-const removeServiceFromInvitation = async(iid, sid, oid, correlationId) => {
-  return Promise.resolve();
-};
-
-const getPageOfPoliciesForService = async (sid, page, pageSize, correlationId) => {
-  return Promise.resolve({
-    policies: {
-      id: 'policyId',
-      applicationId: 'serviceId',
-      name: 'policy name',
-      status: {
-        id: '1',
+        },
       },
-      conditions: [],
-      roles: [],
+    ],
+  },
+];
+const getServicesForUser = (id, correlationId) => {
+  const userServices = services.find((x) => x.userId.toLowerCase() === id.toLowerCase());
+  return Promise.resolve(userServices);
+};
+
+const getSingleUserService = (id, sid, oid, correlationId) => Promise.resolve([]);
+
+const getSingleInvitationService = (iid, sid, oid, correlationId) => Promise.resolve([]);
+
+const listRolesOfService = async (sid, correlationId) => Promise.resolve([]);
+
+const updateUserService = async (uid, sid, oid, role, correlationId) => Promise.resolve(null);
+
+const updateInvitationService = async (iid, sid, oid, role, correlationId) => Promise.resolve(null);
+
+const removeServiceFromUser = async (uid, sid, oid, correlationId) => Promise.resolve();
+
+const removeServiceFromInvitation = async (iid, sid, oid, correlationId) => Promise.resolve();
+
+const getPageOfPoliciesForService = async (sid, page, pageSize, correlationId) => Promise.resolve({
+  policies: {
+    id: 'policyId',
+    applicationId: 'serviceId',
+    name: 'policy name',
+    status: {
+      id: '1',
     },
-    page: page,
-    totalNumberOfPages: 1,
-    totalNumberOfRecords: 1,
-  });
-};
+    conditions: [],
+    roles: [],
+  },
+  page,
+  totalNumberOfPages: 1,
+  totalNumberOfRecords: 1,
+});
 
-const getPolicyById = async (sid, pid, correlationId) => {
-  return Promise.resolve();
-};
+const getPolicyById = async (sid, pid, correlationId) => Promise.resolve();
 
-const addUserService = async (uid, sid, oid, role, correlationId) => {
-  return Promise.resolve(null);
-};
+const addUserService = async (uid, sid, oid, role, correlationId) => Promise.resolve(null);
 
-const addInvitationService = async (iid, sid, oid, role, correlationId) => {
-  return Promise.resolve(null);
-};
+const addInvitationService = async (iid, sid, oid, role, correlationId) => Promise.resolve(null);
 
 module.exports = {
   getServicesForUser,
