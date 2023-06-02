f7daaad21791355c283c1f9cb7d9867983b3deec
Jared Marsh <jared.marsh@education.gov.uk>
2023-06-02 23:01:17 +0100

    Implemented clientId uniqueness check
    Will fail validation if the clientId has been changed and is not unique.

--- src/app/services/serviceConfig.js
+++ src/app/services/serviceConfig.js
@@ -96,6 +96,9 @@ const validate = async (req) => {
     model.validationMessages.clientId = 'Client Id must be 50 characters or less';
   } else if (!/^[A-Za-z0-9-]+$/.test(model.service.clientId)) {
     model.validationMessages.clientId = 'Client Id must only contain letters, numbers, and hyphens';
+  } else if (model.service.clientId !== service.relyingParty.client_id && getServiceById(model.service.clientId, req.id)) {
+    // If getServiceById returns truthy, then that clientId is already in use.
+    model.validationMessages.clientId = 'Client Id is unavailable, please try another';
   }
 
   if (!urlValidation.test(model.service.postResetUrl) && model.service.postResetUrl.trim() !== '') {
