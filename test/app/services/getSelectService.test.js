f45cfc378822499af177e3c5a69ee94ec728e202
Jared Marsh <jared.marsh@education.gov.uk>
2023-08-01 14:44:52 +0100

    getSelectService test eslint fixes

--- test/app/services/getSelectService.test.js
+++ test/app/services/getSelectService.test.js
@@ -1,63 +1,49 @@
-jest.mock('./../../../src/infrastructure/config', () => require('./../../utils').configMockFactory());
-jest.mock('./../../../src/infrastructure/logger', () => require('./../../utils').loggerMockFactory());
+const mockUtils = require('../../utils');
+
+const mockConfig = mockUtils.configMockFactory();
+const mockLogger = mockUtils.loggerMockFactory();
+
+jest.mock('./../../../src/infrastructure/config', () => mockConfig);
+jest.mock('./../../../src/infrastructure/logger', () => mockLogger);
 jest.mock('./../../../src/infrastructure/applications');
 
-const { getRequestMock, getResponseMock } = require('./../../utils');
-const getSelectService = require('./../../../src/app/services/selectService').get;
-const { getServiceById } = require('./../../../src/infrastructure/applications');
+const { getRequestMock, getResponseMock } = require('../../utils');
+const getSelectService = require('../../../src/app/services/selectService').get;
+const { getServiceById } = require('../../../src/infrastructure/applications');
+
 const res = getResponseMock();
 
-describe('when getting the select service page', () => {
+describe('When going to the select-service page', () => {
   let req;
 
   beforeEach(() => {
     req = getRequestMock({
-      params: {
-        sid: 'service1'
-      },
       userServices: {
         roles: [
           {
-          code: 'serviceid_serviceconfiguration'
+            code: 'serviceid_serviceconfig',
           },
           {
-            code: 'serviceid1_serviceconfiguration'
+            code: 'serviceid1_serviceconfig',
           },
-
-        ]
+        ],
       },
     });
 
     getServiceById.mockReset();
-    getServiceById.mockReturnValue({
+    getServiceById.mockReturnValue([{
       id: 'serviceid',
       name: 'service one',
-      description: 'service description',
-      relyingParty: {
-        client_id: 'clientid',
-        client_secret: 'dewier-thrombi-confounder-mikado',
-        api_secret: 'dewier-thrombi-confounder-mikado',
-        service_home: 'https://www.servicehome.com',
-        postResetUrl: 'https://www.postreset.com',
-        redirect_uris: [
-          'https://www.redirect.com',
-        ],
-        post_logout_redirect_uris: [
-          'https://www.logout.com',
-        ],
-        grant_types: [
-          'implicit',
-          'authorization_code'
-        ],
-        response_types: [
-          'code',
-        ],
-      }
-    });
+      description: 'service one description',
+    }], [{
+      id: 'serviceid1',
+      name: 'service two',
+      description: 'service two description',
+    }]);
     res.mockResetAll();
   });
-  it('then it should get the services by id', async () => {
 
+  it('then it should get the services by id', async () => {
     await getSelectService(req, res);
 
     expect(getServiceById.mock.calls).toHaveLength(2);
@@ -66,15 +52,15 @@ describe('when getting the select service page', () => {
   });
 
   it('then it should redirect to the service if only one service', async () => {
-    req.userServices= {
+    req.userServices = {
       roles: [{
-        code: 'serviceid_serviceconfiguration'
-      }]
+        code: 'serviceid_serviceconfiguration',
+      }],
     };
     await getSelectService(req, res);
 
     expect(res.redirect.mock.calls).toHaveLength(1);
-    expect(res.redirect.mock.calls[0][0]).toBe(`serviceid`);
+    expect(res.redirect.mock.calls[0][0]).toBe('serviceid');
   });
 
   it('then it should return the multiple services view', async () => {
@@ -98,12 +84,12 @@ describe('when getting the select service page', () => {
     expect(res.render.mock.calls[0][1]).toMatchObject({
       services: [
         {
-        id: 'serviceid',
-        name: 'service one'
+          id: 'serviceid',
+          name: 'service one',
         },
         {
           id: 'serviceid1',
-          name: 'service one'
+          name: 'service one',
         },
       ],
     });
