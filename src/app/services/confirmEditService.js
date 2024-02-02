1b455dc275dc5d450969e0a42b80f692ea73d6cb
stottidfe <peter.stott@education.gov.uk>
2024-02-02 14:43:20 +0000

    fixed quotes

--- src/app/services/confirmEditService.js
+++ src/app/services/confirmEditService.js
@@ -1,8 +1,8 @@
-const { getServiceById } = require('../../infrastructure/applications');
-const { listRolesOfService, updateUserService, updateInvitationService } = require('../../infrastructure/access');
-const { getUserDetails, getUserServiceRoles } = require('./utils');
-const { getOrganisationByIdV2 } = require('../../infrastructure/organisations');
-const logger = require('../../infrastructure/logger');
+const { getServiceById } = require("../../infrastructure/applications");
+const { listRolesOfService, updateUserService, updateInvitationService } = require("../../infrastructure/access");
+const { getUserDetails, getUserServiceRoles } = require("./utils");
+const { getOrganisationByIdV2 } = require("../../infrastructure/organisations");
+const logger = require("../../infrastructure/logger");
 
 const getModel = async (req) => {
   const service = await getServiceById(req.params.sid, req.id);
@@ -23,19 +23,19 @@ const getModel = async (req) => {
     organisation,
     serviceId: req.params.sid,
     userRoles: manageRolesForService,
-    currentNavigation: 'users',
+    currentNavigation: "users",
   };
 };
 
 const updateService = async (uid, sid, oid, selectedRoles, reqId) => {
-  const update = uid.startsWith('inv-') ? await updateInvitationService(uid.substr(4), sid, oid, selectedRoles, reqId) : updateUserService(uid, sid, oid, selectedRoles, reqId);
+  const update = uid.startsWith("inv-") ? await updateInvitationService(uid.substr(4), sid, oid, selectedRoles, reqId) : updateUserService(uid, sid, oid, selectedRoles, reqId);
   return update;
 };
 
 const get = async (req, res) => {
   const model = await getModel(req);
 
-  return res.render('services/views/confirmEditService', model);
+  return res.render("services/views/confirmEditService", model);
 };
 
 const post = async (req, res) => {
@@ -46,22 +46,22 @@ const post = async (req, res) => {
   await updateService(req.params.uid, req.params.sid, req.params.oid, selectedRoles, req.id);
 
   logger.audit(`${req.user.email} (id: ${req.user.sub}) updated service ${model.service.name} for organisation ${model.organisation.name} (id: ${model.organisation.id}) for user ${model.user.email} (id: ${model.user.id})`, {
-    type: 'manage',
-    subType: 'user-service-updated',
+    type: "manage",
+    subType: "user-service-updated",
     userId: req.user.sub,
     userEmail: req.user.email,
     editedUser: model.user.id,
     editedFields: [
       {
-        name: 'update_service',
+        name: "update_service",
         newValue: selectedRoles,
       },
     ],
   });
 
-  res.flash('title', 'Success');
-  res.flash('heading', `Service updated: ${model.service.name}`);
-  res.flash('message', 'Approvers at the relevant organisation have been notified of this change.');
+  res.flash("title", "Success");
+  res.flash("heading", `Service updated: ${model.service.name}`);
+  res.flash("message", "Approvers at the relevant organisation have been notified of this change.");
 
   return res.redirect(`/services/${req.params.sid}/users/${req.params.uid}/organisations`);
 };
