ca87aa1d0ab52057bcb0914c50c1485e183ba93f
Adam Mann <Adam.Mann@education.gov.uk>
2025-01-27 11:48:47 +0000

    Improve validation and UI elements

--- src/app/services/postCreatePolicyCondition.js
+++ src/app/services/postCreatePolicyCondition.js
@@ -1,5 +1,6 @@
 const { getPolicyById } = require("../../infrastructure/access");
 const { getServiceById } = require("../../infrastructure/applications");
+const { validate: validateUUID } = require("uuid");
 //const logger = require("./infrastructure/logger");
 
 const validate = async (req) => {
@@ -21,12 +22,43 @@ const validate = async (req) => {
   if (!model.value) {
     model.validationMessages.value = "Please enter a value";
   }
-  // TODO depending on condition, the validation changes
-  // if condition = organisation.id, value must be a uuid
-  // if condition = organisation.status
-  // if condition = organisation.urn
-  // if condition = organisation.type
-  // if condition = organisation.category
+
+  if (model.condition && model.operator && model.value) {
+    if (model.condition === "organisation.urn") {
+      const urnRegex = /^\d{6}$/i;
+      if (!urnRegex.test(model.value)) {
+        model.validationMessages.value =
+          "organisation.urn can only be a 6 digit number";
+      }
+    }
+    if (model.condition === "organisation.id") {
+      if (!validateUUID(model.value)) {
+        model.validationMessages.value =
+          "organisation.id needs to be a valid uuid";
+      }
+    }
+    if (model.condition === "organisation.type") {
+      const typeRegex = /^\d{2}$/i;
+      if (!typeRegex.test(model.value)) {
+        model.validationMessages.value =
+          "organisation.type can only be a 2 digit number";
+      }
+    }
+    if (model.condition === "organisation.status") {
+      const statusRegex = /^\d{1,2}$/i;
+      if (!statusRegex.test(model.value)) {
+        model.validationMessages.value =
+          "organisation.status can only be a 1 or 2 digit number";
+      }
+    }
+    if (model.condition === "organisation.category") {
+      const categoryRegex = /^\d{3}$/i;
+      if (!categoryRegex.test(model.value)) {
+        model.validationMessages.value =
+          "organisation.category can only be a 3 digit number";
+      }
+    }
+  }
 
   // TODO Look at current policy and ensure added policy isn't a duplicate
   // TODO check added policy doesn't contradict current policy (e.g., adding is and is not for same condition and value)
@@ -41,10 +73,8 @@ const postCreatePolicyCondition = async (req, res) => {
   const policy = await getPolicyById(req.params.sid, req.params.pid, req.id);
 
   if (Object.keys(model.validationMessages).length > 0) {
-    model.csrfToken = req.csrfToken();
-
     return res.render("services/views/createPolicyCondition", {
-      validationMessages: {},
+      ...model,
       csrfToken: req.csrfToken(),
       policy,
       service,
@@ -64,7 +94,7 @@ const postCreatePolicyCondition = async (req, res) => {
       model.validationMessages.condition =
         "Something went wrong submitting data, please try again";
       return res.render("services/views/createPolicyCondition", {
-        validationMessages: {},
+        ...model,
         csrfToken: req.csrfToken(),
         policy,
         service,
