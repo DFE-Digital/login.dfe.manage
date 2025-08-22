2ae5aaa54c6495026f33221173dd4a10ca95ce18
Adam Mann <Adam.Mann@education.gov.uk>
2025-08-22 15:31:48 +0100

    Commit progress

--- src/app/services/postConfirmEditServiceInfo.js
+++ src/app/services/postConfirmEditServiceInfo.js
@@ -4,13 +4,13 @@ const {
   getServiceById,
   updateService,
 } = require("../../infrastructure/applications");
+const { listRolesOfService } = require("../../infrastructure/access");
+const config = require("../../infrastructure/config");
 
 const postConfirmEditServiceInfo = async (req, res) => {
   const model = req.session.editServiceInfo;
   if (!model) {
-    // TODO figure out where to redirect too
-    logger.info("No model, redirecting away");
-    return res.redirect("edit");
+    return res.redirect(`/services/${req.params.sid}/service-information/`);
   }
 
   // TODO pass existing name and desc (maybe id) in session to stop us having
@@ -24,32 +24,41 @@ const postConfirmEditServiceInfo = async (req, res) => {
       (service) => service.name === model.name,
     );
     if (isMatchingName) {
-      // TODO figure out how to handle it if the name changed in the meantime
-      logger.info(
+      // TODO service-information page needs to handle flash
+      res.flash(
+        "error",
         "Service name must be unique and cannot already exist in DfE Sign-in",
       );
-      return res.redirect(
-        `/services/${req.params.sid}/service-information/edit`,
-      );
+      return res.redirect(`/services/${req.params.sid}/service-information/`);
     }
   }
-
-  // const serviceRoles = Get roles
-  // if (serviceRoles) {
-  //     const internalRoles = find roles ending with service configuation, service banner, Service Access Management and Service Support
-  //     if (internalRoles) {
-  //         Get policy and role data
-  //         Shape it (renaming the start to the new name)
-  //         Patch it
-  //     }
-  //}
 
   const updatedService = {
     name: model.name,
     description: model.description,
   };
   await updateService(req.params.sid, updatedService, req.id);
-  logger.info("Update the role data (if required)");
+  logger.info("Successfully updated service details", {
+    correlationId: req.id,
+  });
+
+  if (service.name !== model.name) {
+    logger.info(
+      "Service name changed.  Updating any internal manage roles for the service",
+      { correlationId: req.id },
+    );
+    // Check for internal manage roles and update service name to new one
+    const manageServiceId = config.access.identifiers.service;
+    const rolesOfService = await listRolesOfService(manageServiceId, req.id);
+    const roles = rolesOfService.filter((role) =>
+      role.name.startsWith(service.name),
+    );
+    console.log(roles);
+    // roles.forEach((role) => {
+    // Amend role name to include new service name
+    // call patch role endpoint
+    // });
+  }
 
   // logger.audit(
   //   `${req.user.email} (id: ${req.user.sub}) updated the name and/or description of service ${req.params.sid}`,
@@ -64,6 +73,7 @@ const postConfirmEditServiceInfo = async (req, res) => {
   // );
 
   req.session.editServiceInfo = undefined;
+  // TODO service-information page doesn't render flash, so need to add that
   res.flash("info", `Successfully updated service name and/or description`);
 
   return res.redirect(`/services/${req.params.sid}/service-information`);
