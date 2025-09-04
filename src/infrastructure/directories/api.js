441dc25b255bd39999782178964123e7a6db5eb5
Ionut Predoi <ionut.predoi@education.gov.uk>
2025-09-04 16:45:50 +0100

    Remove unused `directoriesAPI.getInvitationByEmail` functionality

--- src/infrastructure/directories/api.js
+++ src/infrastructure/directories/api.js
@@ -2,29 +2,6 @@ const config = require("./../config");
 const { fetchApi } = require("login.dfe.async-retry");
 const jwtStrategy = require("login.dfe.jwt-strategies");
 
-const getInvitationByEmail = async (email, correlationId) => {
-  const token = await jwtStrategy(config.directories.service).getBearerToken();
-
-  try {
-    return await fetchApi(
-      `${config.directories.service.url}/invitations/by-email/${email}`,
-      {
-        method: "GET",
-        headers: {
-          authorization: `bearer ${token}`,
-          "x-correlation-id": correlationId,
-        },
-      },
-    );
-  } catch (e) {
-    const status = e.statusCode ? e.statusCode : 500;
-    if (status === 404) {
-      return null;
-    }
-    throw e;
-  }
-};
-
 const createInvite = async (
   firstName,
   lastName,
@@ -111,7 +88,6 @@ const updateInvite = async (id, email, correlationId) => {
 };
 
 module.exports = {
-  getInvitationByEmail,
   createInvite,
   resendInvitation,
   updateInvite,
