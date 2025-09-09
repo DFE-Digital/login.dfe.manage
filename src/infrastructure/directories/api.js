1930edd2740b69fbc660aebbd3eaaf64eb8dbc31
Ionut Predoi <ionut.predoi@education.gov.uk>
2025-09-09 12:34:29 +0100

    Remove  unused `directoriesApi.resendInvitation` functionality

--- src/infrastructure/directories/api.js
+++ src/infrastructure/directories/api.js
@@ -2,29 +2,6 @@ const config = require("./../config");
 const { fetchApi } = require("login.dfe.async-retry");
 const jwtStrategy = require("login.dfe.jwt-strategies");
 
-const resendInvitation = async (id, correlationId) => {
-  const token = await jwtStrategy(config.directories.service).getBearerToken();
-
-  try {
-    await fetchApi(
-      `${config.directories.service.url}/invitations/${id}/resend`,
-      {
-        method: "POST",
-        headers: {
-          authorization: `bearer ${token}`,
-          "x-correlation-id": correlationId,
-        },
-      },
-    );
-    return true;
-  } catch (e) {
-    if (e.statusCode === 404) {
-      return null;
-    }
-    throw e;
-  }
-};
-
 const updateInvite = async (id, email, correlationId) => {
   try {
     const token = await jwtStrategy(
@@ -49,6 +26,5 @@ const updateInvite = async (id, email, correlationId) => {
 };
 
 module.exports = {
-  resendInvitation,
   updateInvite,
 };
