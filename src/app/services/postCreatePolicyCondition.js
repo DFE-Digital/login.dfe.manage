65429b12b4c3b90b42b1593331299d237676213d
Julio Velezmoro <julio28ve@gmail.com>
2025-09-22 07:48:11 +0100

    Replace `getOrganisationById` with ApiClient `getOrganisationRaw`
    Test Suites: 59 passed, 59 total
    Tests:       467 passed, 467 total
    Snapshots:   0 total
    Time:        10.957 s,

--- src/app/services/postCreatePolicyCondition.js
+++ src/app/services/postCreatePolicyCondition.js
@@ -1,5 +1,5 @@
 const { getServicePolicyRaw } = require("login.dfe.api-client/services");
-const { getOrganisationById } = require("../../infrastructure/organisations");
+const { getOrganisationRaw } = require("login.dfe.api-client/organisations");
 const { getUserServiceRoles } = require("./utils");
 const { validate: validateUUID } = require("uuid");
 const logger = require("../../infrastructure/logger");
@@ -101,7 +101,9 @@ const validate = async (req, currentPolicyConditions) => {
   }
 
   if (model.condition === "organisation.id") {
-    const organisation = await getOrganisationById(model.value);
+    const organisation = await getOrganisationRaw({
+      by: { organisationId: model.value },
+    });
     if (!organisation) {
       model.validationMessages.value = "Organisation id does not exist";
     }
