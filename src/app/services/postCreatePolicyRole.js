1a64caff4e6459acfd92eecb08cbf966e363f6c7
Alex Platt <alex.platt@education.gov.uk>
2025-11-28 16:11:32 +0000

    [NSA-9502] Update logging message and tests

--- src/app/services/postCreatePolicyRole.js
+++ src/app/services/postCreatePolicyRole.js
@@ -37,7 +37,7 @@ const validate = async (req) => {
   );
 
   if (existingRoleInPolicy) {
-    model.validationMessages.roleExists = `Role [code: ${model.roleCode}] already exists for this policy`;
+    model.validationMessages.roleExists = `Role with code: ${model.roleCode} already exists for this policy`;
     logger.info(
       `${model.roleName}: [code: ${model.roleCode}] found in policy`,
       { correlationId: req.id },
