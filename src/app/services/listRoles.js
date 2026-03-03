f504d3f6e444b455bbf8bfdc8e2f4a7162ab4253
vsolomonadv <victor.solomon@advania.co.uk>
2026-03-03 09:12:39 +0000

    Complete fix: Return serviceId in viewModel for layout navigation rendering

--- src/app/services/listRoles.js
+++ src/app/services/listRoles.js
@@ -2,47 +2,68 @@ const {
   getServiceRaw,
   getServicePoliciesRaw,
 } = require("login.dfe.api-client/services");
+const { getUserServiceRoles } = require("./utils");
 
 const viewModel = async (req) => {
-  const serviceDetails = await getServiceRaw({
-    by: { serviceId: req.params.sid },
-  });
+  try {
+    // Support both parameter names for compatibility
+    const serviceId = req.params.serviceId || req.params.sid;
+    
+    const serviceDetails = await getServiceRaw({
+      by: { serviceId },
+    });
 
-  const servicePolicies = await getServicePoliciesRaw({
-    serviceId: req.params.sid,
-  });
+    const servicePolicies = await getServicePoliciesRaw({
+      serviceId,
+    });
 
-  // Deduplicate service roles if they occur for more than one policy
-  const uniqueRoles = {};
+    // Deduplicate service roles if they occur for more than one policy
+    const uniqueRoles = {};
 
-  // Check each policy
-  for (const policy of servicePolicies) {
-    // Check each role in that policy
-    for (const role of policy.roles) {
-      // Has the role been added already?
-      if (uniqueRoles[role.id]) {
-        // If it has, push the current policy
-        uniqueRoles[role.id].policies.push(policy.name);
-      } else {
-        // If hasn't, create new role and add current policy
-        uniqueRoles[role.id] = { ...role, policies: [policy.name] };
+    // Check each policy
+    for (const policy of servicePolicies) {
+      // Check each role in that policy
+      for (const role of policy.roles) {
+        // Has the role been added already?
+        if (uniqueRoles[role.id]) {
+          // If it has, push the current policy
+          uniqueRoles[role.id].policies.push(policy.name);
+        } else {
+          // If hasn't, create new role and add current policy
+          uniqueRoles[role.id] = { ...role, policies: [policy.name] };
+        }
       }
     }
+
+    const roles = Object.values(uniqueRoles);
+    const manageRolesForService = await getUserServiceRoles(req);
+
+    return {
+      csrfToken: req.csrfToken(),
+      backLink: true,
+      roles,
+      serviceDetails,
+      serviceId, // ← IMPORTANT: Return serviceId for layout template
+      userRoles: manageRolesForService,
+      currentNavigation: "roles",
+    };
+  } catch (error) {
+    console.error("ERROR in listRoles viewModel:", error.message);
+    throw error;
   }
-
-  const roles = Object.values(uniqueRoles);
-
-  return {
-    csrfToken: req.csrfToken(),
-    backLink: true,
-    roles,
-    serviceDetails,
-  };
 };
 
 const getListRoles = async (req, res) => {
-  const model = await viewModel(req);
-  return res.render("services/views/listRoles", model);
+  try {
+    const model = await viewModel(req);
+    return res.render("services/views/listRoles", model);
+  } catch (error) {
+    console.error("ERROR in getListRoles:", error);
+    return res.status(500).render("error", { 
+      message: error.message,
+      error: error 
+    });
+  }
 };
 
 module.exports = getListRoles;
