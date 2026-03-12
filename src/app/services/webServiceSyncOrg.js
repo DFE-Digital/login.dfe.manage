a28e5eeca8f26aa6df0ee2d668a971a4b2ed2cdf
Ramin Ahmadi <15119083+AhmadiRamin@users.noreply.github.com>
2026-03-12 11:48:23 +0000

    [NSA-9697] Display success message when sync completed successfully

--- src/app/services/webServiceSyncOrg.js
+++ src/app/services/webServiceSyncOrg.js
@@ -1,6 +1,7 @@
 const { getOrganisationRaw } = require("login.dfe.api-client/organisations");
 const { getUserServiceRoles } = require("./utils");
 const { wsSyncCall } = require("./wsSynchFunCall");
+const logger = require("../../infrastructure/logger");
 
 const get = async (req, res) => {
   const organisation = await getOrganisationRaw({
@@ -18,7 +19,20 @@ const get = async (req, res) => {
   });
 };
 const post = async (req, res) => {
-  await wsSyncCall(req.params.oid);
+  try {
+    const result = await wsSyncCall(req.params.oid);
+    if (result?.status === "success") {
+      res.flash("success", "Web service sync completed successfully");
+    } else {
+      res.flash("info", "No data was available to sync for this organisation");
+      logger.info(
+        `Sync call returned unexpected result for org [${req.params.oid}].`,
+      );
+    }
+  } catch (e) {
+    res.flash("error", "Something went wrong during web service sync");
+    logger.error("Something went wrong during web service sync", e);
+  }
 
   return res.redirect(
     `/services/${req.params.sid}/organisations/${req.params.oid}/users`,
