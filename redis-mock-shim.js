19bd7bec0ea22061a56c0ab5784a13abbb46cc13
vsolomonadv <victor.solomon@advania.co.uk>
2026-05-15 19:06:10 +0100

    Remove redis-mock-shim functionality

--- redis-mock-shim.js
+++ redis-mock-shim.js
@@ -1,9 +1,1 @@
-const Module = require("module");
-const originalLoad = Module._load;
 
-Module._load = function (request, parent, isMain) {
-  if (request === "ioredis") {
-    return require("ioredis-mock");
-  }
-  return originalLoad.apply(this, arguments);
-};
